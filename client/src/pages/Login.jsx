9f0e36e85d59348656d14a100983866a7a124bb0
Ackfort <phattarakorn23228@gmail.com>
2024-02-29 18:08:46 +0700

    register UI

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,8 +5,8 @@ const Login = () => {
     return <div className="auth">
         <h1>Login</h1>
         <form>
-            <input type="text" placeholder="username" />
-            <input type="text" placeholder="password" />
+            <input required type="text" placeholder="username" />
+            <input required type="password" placeholder="password" />
             <button>Login</button>
             <p>This is error</p>
             <span>Don't you have an account? <Link to="/register" >Register</Link></span>
