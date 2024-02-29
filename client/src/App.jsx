815927ab6c1adcc982ca40fc935e0a4dbbf22420
Ackfort <phattarakorn23228@gmail.com>
2024-02-29 17:16:16 +0700

    createBrowserRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+import Login from "./pages/Login";
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet/>
+      <Footer />
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Layout/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/post/:id",
+        element: <Single/>
+      },
+      {
+        path: "/write",
+        element: <Write/>
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+]);
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
