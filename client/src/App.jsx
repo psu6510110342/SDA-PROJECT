12f33415c1ba5b704737e753124192c8352cda03
Ackfort <phattarakorn23228@gmail.com>
2024-03-01 07:56:55 +0700

    createBrowserRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,28 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>hello</div>
+  },
+  {
+    path: "/register",
+    element: <Register/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+]);
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
