a4565824935953a6b9702a90187a66dac6905709
Ackfort <phattarakorn23228@gmail.com>
2024-03-05 13:28:34 +0700

    footer UI

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,14 +1,35 @@
 import React from "react";
-import Logo from "../img/logo.png";
 
 const Footer = () => {
     return (
-        <footer>
-            <img src={Logo} alt="" />
-            <span>
-                Made with ♥️ and <b>React.js</b>.
-            </span>
-        </footer>
+        <div className="container my-5">
+            <footer className="text-center text-white" style={{ backgroundColor: "#112D4E" }}>
+                <div className="container">
+                    <section className="mt-5">
+                    </section>
+                    <hr className="my-5" />
+
+                    <section className="mb-5">
+                        <div className="row d-flex justify-content-center">
+                            <div className="col-lg-8">
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+                                    distinctio earum repellat quaerat voluptatibus placeat nam,
+                                    commodi optio pariatur est quia magnam eum harum corrupti
+                                    dicta, aliquam sequi voluptate quas.
+                                </p>
+                            </div>
+                        </div>
+                    </section>
+
+                </div>
+                <div
+                    className="text-center p-3"
+                    style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
+                    SAD-PROJECT
+                </div>
+            </footer>
+        </div>
     );
 }
 
