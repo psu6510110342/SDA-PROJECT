7161b5aa3c84362ad120b4c53c363edf55460ae0
Ackfort <phattarakorn23228@gmail.com>
2024-03-05 10:52:50 +0700

    Navbar UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,49 +1,34 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import React from "react";
-import Logo from "../img/logo.png";
-import { Link } from "react-router-dom";
 
-const Navbar = () => {
+function Navbarr() {
     return (
-        <div className="navbar">
-            <div className="container">
-                <div className="logo">
-                    <Link to="/">
-                        <img src={Logo} alt="" />
-                    </Link>
-                </div>
-                <div className="links">
-                    <Link className="link" to="/?cat=art">
-                        <h6>ART</h6>
-                    </Link>
-                    <Link className="link" to="/?cat=science">
-                        <h6>SCIENCE</h6>
-                    </Link>
-                    <Link className="link" to="/?cat=technology">
-                        <h6>TECHNOLOGY</h6>
-                    </Link>
-                    <Link className="link" to="/?cat=cinema">
-                        <h6>CINEMA</h6>
-                    </Link>
-                    <Link className="link" to="/?cat=design">
-                        <h6>DESIGN</h6>
-                    </Link>
-                    <Link className="link" to="/?cat=food">
-                        <h6>FOOD</h6>
-                    </Link>
-                    <Link className="link" to="/login">
-                        Login
-                    </Link>
-                    <span>John</span>
-                    <span>Logout</span>
-                    <span className="write">
-                        <Link className="link" to="/write">
-                            Write
-                        </Link>
-                    </span>
-                </div>
-            </div>
+        <div className='nav-new'>
+            <Navbar collapseOnSelect expand="lg" className="navbar navbar-dark bg-transparent">
+                <Container className='py-2 px-3' style={{ backgroundColor: "#112D4E" }}>
+                    <Navbar.Brand href="#home">BLOG-APP</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/?cat=art">ART</Nav.Link>
+                            <Nav.Link href="/?cat=science">SCIENCE</Nav.Link>
+                            <Nav.Link href="/?cat=technology">TECHNOLOGY</Nav.Link>
+                            <Nav.Link href="/?cat=cinema">CINEMA</Nav.Link>
+                            <Nav.Link href="/?cat=design">DESIGN</Nav.Link>
+                            <Nav.Link href="/?cat=food">FOOD</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="/write">Logout</Nav.Link>
+                            <Nav.Link href="/write">Login</Nav.Link>
+                            <Nav.Link href="/write">Write</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 }
 
-export default Navbar
+export default Navbarr;
